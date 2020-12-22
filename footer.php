01719704e367f89954a72611dec15164f6359590
Jahnvi Mandle <67870408+mandlejahnvi@users.noreply.github.com>
2020-12-22 12:47:16 +0530

    Add files via upload
    footer file

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer id="footer">
+    <p class="copyright">Copyright &copy; 2020 Company Name
+     <br>Designed by <a rel="nofollow" href="https://www.facebook.com/templatemo">Jahnvi Mandle</a></p>
+</footer>
+
+            
+            
+              
+  
